df1ff63b4ef488a1370fab583fb768f677507326
Donnie Tolbert <donniet@gmail.com>
2019-11-04 15:46:20 -0600

    added text file output

--- nvidia_smi_exporter.go
+++ nvidia_smi_exporter.go
@@ -1,66 +1,141 @@
 package main
 
 import (
-    "bytes"
-    "encoding/csv"
-    "fmt"
-    "net/http"
-    "log"
-    "os"
-    "os/exec"
-    "strings"
+	"bytes"
+	"context"
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+	"os/signal"
+	"strings"
+	"time"
 )
 
+var (
+	addr       = ":9100"
+	textPath   = ""
+	textUpdate = 5000 * time.Millisecond
+)
+
+func init() {
+	flag.StringVar(&addr, "addr", addr, "addr to listen on for HTTP")
+	flag.StringVar(&textPath, "textPath", textPath, "path to write text profiling to.  Set this and we won't have an http handler")
+	flag.DurationVar(&textUpdate, "textUpdate", textUpdate, "time between updating analytics")
+}
 
 // name, index, temperature.gpu, utilization.gpu,
 // utilization.memory, memory.total, memory.free, memory.used
 
 func metrics(response http.ResponseWriter, request *http.Request) {
-    out, err := exec.Command(
-        "nvidia-smi",
-        "--query-gpu=name,index,temperature.gpu,utilization.gpu,utilization.memory,memory.total,memory.free,memory.used",
-        "--format=csv,noheader,nounits").Output()
+	if err := writeMetrics(response); err != nil {
+		response.WriteHeader(500)
+		fmt.Fprintf(response, "%v", err)
+		log.Printf("%v", err)
+	}
+}
 
-    if err != nil {
-        fmt.Printf("%s\n", err)
-        return
-    }
+func writeMetrics(w io.Writer) error {
+	out, err := exec.Command(
+		"nvidia-smi",
+		"--query-gpu=name,index,temperature.gpu,utilization.gpu,utilization.memory,memory.total,memory.free,memory.used",
+		"--format=csv,noheader,nounits").Output()
 
-    csvReader := csv.NewReader(bytes.NewReader(out))
-    csvReader.TrimLeadingSpace = true
-    records, err := csvReader.ReadAll()
+	if err != nil {
+		return err
+	}
 
-    if err != nil {
-        fmt.Printf("%s\n", err)
-        return
-    }
+	csvReader := csv.NewReader(bytes.NewReader(out))
+	csvReader.TrimLeadingSpace = true
+	records, err := csvReader.ReadAll()
 
-    metricList := []string {
-        "temperature.gpu", "utilization.gpu",
-        "utilization.memory", "memory.total", "memory.free", "memory.used"}
+	if err != nil {
+		return err
+	}
 
-    result := ""
-    for _, row := range records {
-        name := fmt.Sprintf("%s[%s]", row[0], row[1])
-        for idx, value := range row[2:] {
-            result = fmt.Sprintf(
-                "%s%s{gpu=\"%s\"} %s\n", result,
-                metricList[idx], name, value)
-        }
-    }
+	metricList := []string{
+		"temperature.gpu", "utilization.gpu",
+		"utilization.memory", "memory.total", "memory.free", "memory.used"}
 
-    fmt.Fprintf(response, strings.Replace(result, ".", "_", -1))
+	result := ""
+	for _, row := range records {
+		name := fmt.Sprintf("%s[%s]", row[0], row[1])
+		for idx, value := range row[2:] {
+			result = fmt.Sprintf(
+				"%s%s{gpu=\"%s\"} %s\n", result,
+				metricList[idx], name, value)
+		}
+	}
+
+	fmt.Fprintf(w, strings.Replace(result, ".", "_", -1))
+	return nil
 }
 
 func main() {
-    addr := ":9101"
-    if len(os.Args) > 1 {
-        addr = ":" + os.Args[1]
-    }
+	flag.Parse()
 
-    http.HandleFunc("/metrics/", metrics)
-    err := http.ListenAndServe(addr, nil)
-    if err != nil {
-        log.Fatal("ListenAndServe: ", err)
-    }
+	sig := make(chan os.Signal)
+
+	signal.Notify(sig, os.Interrupt)
+
+	if textPath == "" {
+		mux := http.NewServeMux()
+
+		mux.HandleFunc("/metrics/", metrics)
+
+		srv := &http.Server{
+			Addr:    addr,
+			Handler: mux,
+		}
+
+		go func() {
+			if err := srv.ListenAndServe(); err != nil {
+				log.Fatal(err)
+			}
+		}()
+
+		<-sig
+
+		srv.Shutdown(context.Background())
+
+		return
+	}
+
+outer_loop:
+	for {
+		select {
+		case <-sig:
+			break outer_loop
+		default:
+		}
+
+		var file io.WriteCloser
+
+		if textPath == "-" {
+			file = os.Stdout
+		} else {
+			var err error
+			file, err = os.OpenFile(textPath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0660)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+
+		writeMetrics(file)
+
+		if textPath != "-" {
+			file.Close()
+		}
+
+		timer := time.NewTimer(textUpdate)
+		select {
+		case <-sig:
+			break outer_loop
+		case <-timer.C:
+		}
+	}
 }
